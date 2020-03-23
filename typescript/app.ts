76dd402918ff277afa2c7c8579a74ae3ba2fe5f8
rashier <rashier13@gmail.com>
2020-03-23 17:12:49 +0100

    prueba hecha

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,23 +1,26 @@
 (()=>{
 
   // Uso de Let y Const
-  var nombre = 'Ricardo Tapia';
-  var edad = 23;
+  let nombre = 'Ricardo Tapia';
+  const edad = 23;
 
-  var PERSONAJE = {
-    nombre: nombre,
-    edad: edad
-  };
+  const PERSONAJE = { nombre, edad };
 
+  console.log(`${personaje.nombre} ${personaje.edad}`)
 
   // Cree una interfaz que sirva para validar el siguiente objeto
-  var batman = {
+  interface Batman{
+    nombre: string;
+    artesMarciales: string[];
+  }
+  
+  const batman: Batman = {
     nombre: 'Bruno Díaz',
     artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
   }
 
   // Convertir esta funcion a una funcion de flecha
-  function resultadoDoble( a, b ){
+  const resultadoDoble = ( a:number, b: number ): number => {
     return (a + b) * 2
   }
 
@@ -25,12 +28,12 @@
   // donde NOMBRE = obligatorio
   //       PODER  = opcional
   //       ARMA   = por defecto = 'arco'
-  function getAvenger( nombre, poder, arma ){
-    var mensaje;
+  function getAvenger( nombre: string, poder?: string, arma: string = 'arco' ){
+    let mensaje;
     if( poder ){
       mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
     }else{
-      mensaje = nombre + ' tiene un ' + poder
+      mensaje = nombre + ' tiene un ' + poder; 
     }
   };
 
@@ -42,6 +45,10 @@
   // También un método que calcule el área  =  base * altura,
   // ese método debe de retornar un numero.
 
-
+  class Rectangulo {
+    constructor(public base: number, public altura: number){
+    }
+    calcularArea = ():number => this.base * this.altura;
+  }
 
 })();
